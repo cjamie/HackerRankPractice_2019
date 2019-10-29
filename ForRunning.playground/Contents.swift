293dd9b09b5f7447ce3bb06fd9fe021caa8db602
Jamie <jamiechu9288@gmail.com>
2019-10-29 10:11:48 -0700

    extra update (don't know what i added)

--- ForRunning.playground/Contents.swift
+++ ForRunning.playground/Contents.swift
@@ -1,46 +1,25 @@
 import Foundation
 
-//https://stackoverflow.com/questions/45504400/regex-match-pattern-of-alternating-characters
-private let alternatingRegex = "^([a-z])(?!\\1)([a-z])(?:\\1\\2)*\\1?$"
-
 extension String {
-    var isAlternating: Bool {
-        return matches(alternatingRegex)
-    }
     
-    func matches(_ regex: String) -> Bool {
-        return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
-    }
 }
 
-let inputString = "beabeefeab"
 
-// we need to change it so that we only include two of the characters
-let characterSet = Array(Set(inputString))
-
-// this will iterate though input string and only include the int
-func process(first: Character, second: Character) -> Int? {
-    // we can allow access to this through a proxy
-    
-    let value = inputString.reduce("") { counter, next in
-        if next == first || next == second {
-            return counter + String(next)
-        } else {
-            return counter
+extension Character {
+    var isASCIIShiftable: Bool {
+        guard let value = asciiValue else {
+            return false
         }
-    }
-    
-    return value.isAlternating ? value.count : nil
-}
-
-var maxCounter = 0
-
-for i in stride(from: 0, to: characterSet.count, by: 1) {
-    for j in stride(from: i+1, to: characterSet.count, by: 1) {
-        if let newValue = process(first: characterSet[i], second: characterSet[j]) {
-            maxCounter = max(maxCounter, newValue)
-        }
+        
+        let acceptableRanges: [Range<Int>] = [(97...122), (65...90)]
+        return acceptableRanges.contains(where: { range in
+            range.contains(<#T##element: Int##Int#>)
+        })
     }
 }
 
-print(maxCounter)
+let characters: [Character] = ["a", "z", "A", "Z"]
+
+characters.forEach {
+    print("-=- \($0) \($0.asciiValue)")
+}
